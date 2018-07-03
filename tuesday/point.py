1c391efe30abc6c50e8014e949fd315c3a3d0246
Tigran Simonyan <aas@mail.com>
2018-07-03 14:47:20 +0400

    add id field to users get endpoint

--- tuesday/point.py
+++ tuesday/point.py
@@ -3,13 +3,15 @@ from rest_framework import serializers, viewsets
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'is_staff')
+        fields = ('id', 'url', 'username', 'email', 'is_staff')
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.filter(is_staff=False)
     serializer_class = UserSerializer
     http_method_names = ('get', 'post', 'patch', 'put')
     permission_classes = ()
